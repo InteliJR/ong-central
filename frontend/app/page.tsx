04877ac6da92080b0ae6df485d5b88b3c0d40a70
Davi Nascimento de Jesus <davinasc.dejesus@gmail.com>
2024-12-04 03:18:45 -0300

    feat: adiciona carrossel com campanhas

--- frontend/app/page.tsx
+++ frontend/app/page.tsx
@@ -1,11 +1,18 @@
 import Cabecalho from "@/components/nossos_componentes/Cabecalho";
 import Final from "@/components/nossos_componentes/Final";
+import Carrossel from "@/components/nossos_componentes/Carrossel";
+
+const images = [
+  '/Campanha2.png',
+  '/Campanha1.png'
+];
 
 export default function Home() {
   return (
     <>
-      <Cabecalho></Cabecalho>
-      <Final></Final>
+      <Cabecalho />
+      <Carrossel images={images} title="Campanhas em andamento" />
+      <Final />
     </>
   );
 }
