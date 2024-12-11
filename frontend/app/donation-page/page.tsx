8ac6827fb3b5d021d3d26b56f658349d033f781e
pedropinrodrigues <pedro.rodrigues@sou.inteli.edu.br>
2024-12-11 11:07:52 -0300

    feat: adiciona a integração de email a página de doação

--- frontend/app/donation-page/page.tsx
+++ frontend/app/donation-page/page.tsx
@@ -1,3 +1,4 @@
+"use client"; // <-- Converte para Client Component
 import Image from 'next/image'
 import { Card } from "@/components/shadcnui/card"
 import { Button } from "@/components/shadcnui/button"
@@ -6,8 +7,28 @@ import { Textarea } from "@/components/shadcnui/textarea"
 import { RadioGroup, RadioGroupItem } from "@/components/shadcnui/radio-group"
 import { Label } from "@/components/shadcnui/label"
 import Header from '../../components/nossos_componentes/Header'
+import emailjs from '@emailjs/browser';
+
+
+const SERVICE_ID = process.env.NEXT_PUBLIC_EMAILJS_SERVICE_ID_DONATION;
+const TEMPLATE_ID = process.env.NEXT_PUBLIC_EMAILJS_TEMPLATE_ID_DONATION;
+const PUBLIC_KEY = process.env.NEXT_PUBLIC_EMAILJS_PUBLIC_KEY;
 
 export default function DonationPage() {
+
+  const handleOnSubmit = (event) => {
+    event.preventDefault();
+    emailjs.sendForm(SERVICE_ID, TEMPLATE_ID, event.target, PUBLIC_KEY)
+      .then((result) => {
+        console.log(result.text);
+        alert('Mensagem enviada com sucesso!');
+      }, (error) => {
+        console.log(error.text);
+        alert('Ocorreu um erro ao enviar a mensagem. Por favor, tente novamente.');
+      });
+    event.target.reset();
+  };
+
   return (
     <div className="min-h-screen bg-white flex flex-col">
       {/* Green Header */}
@@ -77,63 +98,71 @@ export default function DonationPage() {
             Aceitamos <span className="font-black">doações de itens</span>!
           </h3>
 
-          <form className="space-y-6">
+          <form className="space-y-6" onSubmit={handleOnSubmit}>
             <div className="grid md:grid-cols-2 gap-6">
               <div className="space-y-2">
-                <Label htmlFor="item">Qual item quer doar?</Label>
+                <Label htmlFor="item">Item que deseja doar:</Label>
                 <Input
                   id="item"
+                  name="item"
                   placeholder="Camisa, sapato, cobertor..."
                   className="w-full"
+                  required
                 />
               </div>
 
               <div className="space-y-2">
-                <Label htmlFor="email">Indique seu melhor e-mail</Label>
+                <Label htmlFor="email">Seu melhor e-mail:</Label>
                 <Input
                   id="email"
+                  name="email"
                   type="email"
                   placeholder="fulanodetal@gmail.com"
                   className="w-full"
+                  required
                 />
               </div>
             </div>
 
             <div className="space-y-2">
-              <Label htmlFor="description">Escreva detalhadamente o estado do item</Label>
+              <Label htmlFor="descricao">Descrição do estado do item:</Label>
               <Textarea
-                id="description"
+                id="descricao"
+                name="descricao"
                 placeholder="A camisa é do tamanho P infantil vermelha e branca, com estampa de desenho animado, e foi utilizada somente por 1 mês."
                 className="min-h-[100px]"
+                required
               />
             </div>
 
             <div className="space-y-2">
-              <Label>Você pode entregar o seu item em Rua MMDC, São Paulo, Butantã?</Label>
-              <RadioGroup defaultValue="sim">
+              <Label>Você pode entregar o item em Rua MMDC, São Paulo, Butantã?</Label>
+              <RadioGroup name="entrega_local" defaultValue="sim">
                 <div className="flex items-center space-x-2">
-                  <RadioGroupItem value="sim" id="sim" />
+                  <RadioGroupItem value="Sim" id="sim" />
                   <Label htmlFor="sim">Sim</Label>
                 </div>
                 <div className="flex items-center space-x-2">
-                  <RadioGroupItem value="nao" id="nao" />
+                  <RadioGroupItem value="Não" id="nao" />
                   <Label htmlFor="nao">Não</Label>
                 </div>
               </RadioGroup>
             </div>
 
             <div className="space-y-2">
-              <Label htmlFor="address">Qual o seu endereço?</Label>
+              <Label htmlFor="endereco">Seu endereço:</Label>
               <Input
-                id="address"
+                id="endereco"
+                name="endereco"
                 placeholder="Rua Voluntariado, 115, Bairro da Solidariedade, São Paulo-SP, 55555-555"
                 className="w-full"
+                required
               />
             </div>
 
             <div className="flex justify-end">
-              <Button 
-                type="submit" 
+              <Button
+                type="submit"
                 className="w-full md:w-auto bg-red-400 hover:bg-red-500 text-white font-bold py-2 px-4 rounded"
               >
                 Enviar
